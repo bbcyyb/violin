1b958b6e2f272e3ab5353b420b85852421ae3f5c
Kevin Yabing Yu <Kevin.Y.Yu@emc.com>
2020-06-24 14:06:05 +0800

    Test file r/w

--- violin_scraper/main.py
+++ violin_scraper/main.py
@@ -1,16 +1,28 @@
 from scrapy.cmdline import execute
+from violin_scraper.utils import File
+from violin_scraper.utils import running_path
 
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+def run():
+    path = running_path()
+    sys.path.append(path)
+    spiders = []
+    # spiders.append('slct')
+    spiders.append('example')
 
-# os.path.abspath(__file__) get current py file location
-# os.path.dirname() get location of parent
+    for spider_name in spiders:
+        execute(['scrpy', 'crawl', spider_name])
 
-spiders = []
-# spiders.append('slct')
-spiders.append('example')
+def test_file():
+    path = running_path()
+    file = os.path.join(path, 'test.txt')
+    f = File()
+    f.open_file(file=file)
+    f.writeline('hahahaha', True)
+    f.close_file()
 
-for spider_name in spiders:
-    execute(['scrpy', 'crawl', spider_name])
+if __name__ == "__main__":
+    # run() 
+    test_file()
--- violin_scraper/violin_scraper/utils.py
+++ violin_scraper/violin_scraper/utils.py
@@ -1,19 +1,24 @@
 from scrapy import shell
 from enum import Enum
 
+import os
+
 def inspect(response, spider):
     spider.logger.info('========> Start inspecting')
     shell.inspect_response(response, spider)
     spider.logger.info('<======== Finish inspecting')
 
+def running_path():
+    return os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 class File:
     def __init__(self, logger = None):
-        _file = None
-        _logger = logger
+        self._file = None
+        self._logger = logger
         pass
 
-    def open_file(self, file, mode='r', buffering=-1, encoding = 'utf-8', errors = None, newline = None, closefd = True, opener = None):
-        _file = open(file, mode, buffering, encoding, errors, newline, closefd, opener)
+    def open_file(self, file, mode='a', buffering=-1, encoding = 'utf-8', errors = None, newline = None, closefd = True, opener = None):
+        self._file = open(file, mode, buffering, encoding, errors, newline, closefd, opener)
 
     def close_file(self):
         if not self.get_status() == FileStatus.Opened:
@@ -29,13 +34,13 @@ class File:
 
     def write(self, str, auto_flush):
         self._file.write(str)
-        _log(self, str)
+        self._log(str)
         if auto_flush:
             self.flush()
 
     def writeline(self, str, auto_flush):
         strline = '{}\n'.format(str)
-        write(self, strline, auto_flush)
+        self.write(strline, auto_flush)
 
     def flush(self):
         self._file.flush()
